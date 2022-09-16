309df36035598fb93232742badd6a81b839a38a0
Kai Hasse <kai@kastelo.com>
2022-09-16 15:24:41 +0200

    Added all changes for static

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("rango says here is your about page!")
